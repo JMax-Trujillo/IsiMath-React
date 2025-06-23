307df3bf9e6b18c701779446e2e7fdf48ccf6069
maxito <jeanmax052025@gmail.com>
2025-06-23 17:57:24 -0500

    Paso 6: Usando UseState

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/Dashboard.css';
 
 function Dashboard() {
+
+    const [openMenu, setOpenMenu] = useState('none');
+
+    const toggleMenu = (menuName) => {
+        setOpenMenu(prevOpenMenu => prevOpenMenu === menuName ? 'none' : menuName)
+    }
+
     return (
         <div className="dashboard-container">
             <aside className="sidebar">
@@ -11,14 +18,54 @@ function Dashboard() {
                 <nav className="sidebar-nav">
                     <ul className="nav-list">
                         <li className="nav-list">
-                            <button className="nav-button">
+                            <button 
+                                className= {`nav-button ${openMenu === 'funciones' ? 'active' : ''}`}
+                                onClick={() => toggleMenu('funciones')}
+                            >
                                 Funciones
                             </button>
+                            {/* Sub menu de Funciones, se muestra condicionalmente */}
+                            {openMenu === 'funciones' && (
+                                <ul className="sub-nav-list">
+                                    <li className="sub-nav-item">
+                                      <button className="sub-nav-button">Derivadas</button>
+                                    </li>
+                                    <li className="sub-nav-item">
+                                      <button className="sub-nav-button">Integrales Definidas</button>
+                                    </li>
+                                    <li className="sub-nav-item">
+                                      <button className="sub-nav-button">Integrales Indefinidas</button>
+                                    </li>
+                                    <li className="sub-nav-item">
+                                      <button className="sub-nav-button">Límites</button>
+                                    </li>
+                                </ul>
+                            )}
                         </li>
                         <li className="nav-list">
-                            <button className='nav-button'>
+                            <button 
+                                className={`nav-button ${openMenu === 'matrices' ? 'active' : ''}`}
+                                onClick={()=> toggleMenu('matrices')}
+                            >
                                 Matrices
                             </button>
+                            {/* Sub-menu de Matrices, se muestra condicionalmente */}
+                            {openMenu === 'matrices' && (
+                                <ul className="sub-nav-list">
+                                    <li className="sub-nav-item">
+                                        <button className="sub-nav-button">Calculadora Básica</button>
+                                    </li>
+                                    <li className="sub-nav-item">
+                                        <button className="sub-nav-button">Determinante</button>
+                                    </li>
+                                    <li className="sub-nav-item">
+                                        <button className="sub-nav-button">Inversa</button>
+                                    </li>
+                                    <li className="sub-nav-item">
+                                        <button className="sub-nav-button">Métodos Iterativos</button>
+                                    </li>
+                                </ul>
+                            )}
                         </li>
                     </ul>
                 </nav>
