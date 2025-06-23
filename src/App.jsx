ef940579648f9eb155023311d74c93a8ee2543f4
maxito <jeanmax052025@gmail.com>
2025-06-23 16:37:27 -0500

    Paso 2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './styles/global.css'
+
+function App() {
+
+  return (
+    <div className="appContainer">
+      <h1>Hola Mundo Matematico</h1>
+      <p>Estos son los primeros pasos en la app</p>
+    </div>
+  )
+}
+
+export default App
